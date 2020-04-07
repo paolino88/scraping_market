71c6fc407c7a99b47887b8779c986d1fd08a94df
paolino88 <52924945+paolino88@users.noreply.github.com>
2020-04-07 19:20:11 +0200

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -6,11 +6,12 @@ import json
 ii=1
 
 driver = webdriver.Chrome("/PATH/chromedriver")
+time.sleep(2)
 
 driver.get("https://www.supermercato24.it/")
 driver.find_element_by_link_text("ACCETTO").click()
 driver.find_element_by_link_text("Fai la spesa").click()
-time.sleep(7)
+time.sleep(5)
 driver.find_element_by_xpath("//input[@type='email']").send_keys("xxxxxx@xxxxx.it")
 driver.find_element_by_xpath("//input[@type='password']").send_keys("INSERT_PW")
 driver.find_element_by_xpath("//input[@value='Accedi']").click()
@@ -21,11 +22,8 @@ driver.get('https://www.supermercato24.it/s#/locations/11243/stores/4494/checkou
 
 while(ii==1):
     endpoint_orari = "https://api.supermercato24.it/sm/api/v3/locations/11243/stores/4494/availability?funnel=CHECKOUT"
-    try:
-        trigger_status_update()
-    except:
-        driver.refresh()
 
+    driver.refresh()
     request = driver.wait_for_request(endpoint_orari)
     j = json.loads(request.response.body)
     del driver.requests
